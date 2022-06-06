788f4a582441aabf02dc9055817e0fc1db085d5a
duygu2 <duyguorhan017@gmail.com>
2022-06-06 21:08:57 +0300

    changed login page

--- app/src/main/java/com/duyguorhan/yemeksiparisi/CustomerLogin.kt
+++ app/src/main/java/com/duyguorhan/yemeksiparisi/CustomerLogin.kt
@@ -1,11 +1,19 @@
 package com.duyguorhan.yemeksiparisi
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class CustomerLogin : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_customer_login)
+
+        val ButtonCustomerLogin=findViewById<Button>(R.id.CustomerLoginBtn)
+        ButtonCustomerLogin.setOnClickListener {
+            val intent= Intent(this,CustomerRegister::class.java)
+            startActivity(intent)
+        }
     }
 }
